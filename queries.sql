454d98f6454c80ceb8bbeb7649b59eba0e87e95e
sean galloway <sean.galloway@outlook.com>
2020-11-27 15:49:25 -0800

    Did the first part of the bonus

--- queries.sql
+++ queries.sql
@@ -1,19 +1,19 @@
 -- 1. List the following details of each employee: employee number, last name, first name, sex, and salary.
 SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
 FROM employees AS e
-INNER JOIN salaries AS s 
-ON s.emp_no = e.emp_no;
+INNER JOIN salaries AS s
+    ON s.emp_no = e.emp_no;
 
 -- 2. List first name, last name, and hire date for employees who were hired in 1986.
 SELECT first_name, last_name, hire_date
 FROM employees
 WHERE hire_date >= '1986-01-01' AND hire_date <= '1986-12-31';
 
--- 3. List the manager of each department with the following information: 
+-- 3. List the manager of each department with the following information:
 --    department number, department name, the manager's employee number, last name, first name.
 SELECT dm.dept_no, d.dept_name, e.emp_no, e.first_name, e.last_name
 FROM dept_manager AS dm
-JOIN departments AS d 
+JOIN departments AS d
 	ON d.dept_no = dm.dept_no
 JOIN employees AS e
 	ON e.emp_no = dm.emp_no;
@@ -30,7 +30,7 @@ JOIN employees AS e
 -- 5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
 SELECT first_name, last_name, sex
 FROM employees
-WHERE first_name = 'Hercules' and LEFT(last_name,1) = 'B';
+WHERE first_name = 'Hercules' and LEFT(last_name, 1) = 'B';
 
 -- 6. List all employees in the Sales department, including their:
 --    employee number, last name, first name, and department name.
